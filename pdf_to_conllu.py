7e361bc8b23b3e6d49a3c1e11405d8cfd31f1b36
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2025-02-15 14:25:17 +0100

    removes space_after logic because i dont need it

--- pdf_to_conllu.py
+++ pdf_to_conllu.py
@@ -14,7 +14,7 @@ def get_text(pdf_path):
                 clean_text = re.sub(r'\s+', ' ', page_text).strip()
                 text.append(clean_text) 
 
-    return " ".join(text) # Returns text as one big string
+    return ' '.join(text) # Returns text as one big string
 
 def process_text_with_spacy(text, nlp):
     # Splits text into sentences and tokenizes them
@@ -25,15 +25,12 @@ def process_text_with_spacy(text, nlp):
     for sent in doc.sents:
         tokens = []
         for token in sent:
-            space_after = 'SpaceAfter=No' if token.whitespace_ == '' else ''
-
             # Further split tokens (punctuations) to separate symbols
-            #split_tokens = re.findall(r"\w+|[^\w\s]", token.text, re.UNICODE)
             split_tokens = re.findall(r"\w+(?:[-/.]\w+)*|[^\w\s]", token.text, re.UNICODE)
             for sub_token in split_tokens:
-                tokens.append((sub_token, space_after))
+                tokens.append(sub_token)
 
-        sentences.append(tokens) # Each sent is a list of tuples 
+        sentences.append(tokens) # Each sent is a list of tokens
 
     return sentences # Returns a list of sentences
 
@@ -43,18 +40,11 @@ def to_conllu(sentences):
     conllu_lines = []
     for sent_id, tokens in enumerate(sentences, start=1):
         conllu_lines.append(f'# sent_id = {sent_id:06d}') # metadata
-
-        meta_text = []
-        for i, (token, space_after) in enumerate(tokens):
-            meta_text.append(token)
-            if space_after != "SpaceAfter=No" and i < len(tokens) - 1:  
-                meta_text.append(" ")
-        
-        conllu_lines.append(f'# text = {"".join(meta_text)}') # metadata
+        conllu_lines.append(f'# text = {" ".join(tokens)}') # metadata
     
-        for token_id, (token, space_after) in enumerate(tokens, start=1):
+        for token_id, token in enumerate(tokens, start=1):
             lemma = upos = xpos = feats = head = deprel = deps = '_'
-            misc = f'{space_after}|name=O' if space_after else 'name=O'
+            misc = 'name=O'
             conllu_lines.append(
                 f'{token_id}\t{token}\t{lemma}\t{upos}\t{xpos}\t{feats}\t{head}\t{deprel}\t{deps}\t{misc}'
                 )
@@ -68,11 +58,10 @@ def main():
     # The spacy model
     nlp = spacy.load('nb_core_news_md')
 
-    pdf = '000000'
+    pdf = '000001'
 
     pdf_text = get_text(f'data/pdfs/{pdf}.pdf')
     sentences = process_text_with_spacy(pdf_text, nlp)
-
     conllu_data = to_conllu(sentences)
 
     with open(f'data/conllus/{pdf}.conllu', 'w', encoding='utf-8') as f:
