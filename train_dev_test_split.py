143293029f83a6e4f2ca29e9877451f340e02b77
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-02-15 14:37:22 +0100

    init commit

--- train_dev_test_split.py
+++ train_dev_test_split.py
@@ -54,9 +54,13 @@ def split_conllu_train_dev_test(input_file, train_file, dev_file, test_file, tra
     print(f"Test set: {len(test_blocks)} sentences")
 
 if __name__ == '__main__':
+    
     input_file = 'data/all_regplans.conllu'  
     train_file = 'data/regplans-train.conllu'  
     dev_file = 'data/regplans-dev.conllu'  
     test_file = 'data/regplans-test.conllu'  
+
+    train_ratio = 0.7
+    dev_ratio = 10.5
     
-    split_conllu_train_dev_test(input_file, train_file, dev_file, test_file)
+    split_conllu_train_dev_test(input_file, train_file, dev_file, test_file, train_ratio, dev_ratio)
